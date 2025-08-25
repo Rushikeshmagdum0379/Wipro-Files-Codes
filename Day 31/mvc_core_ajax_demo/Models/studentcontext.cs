c38406947fd92148bb10b39661e80fb84f67dd4c
Rushikesh Magdum <116444411+Rushikeshmagdum0379@users.noreply.github.com>
2025-08-25 19:05:52 +0530

    Delete Day 31 directory

--- Day 31/mvc_core_ajax_demo/Models/studentcontext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using mvccore_ajax_demo.Models;
-
-namespace mvccore_ajax_demo.Models;
-
-public class StudentContext : DbContext
-{
-    public DbSet<Student> Students { get; set; }
-}
--- Day 31/repo_Pattern_EFCORE/repo_Pattern_EFCORE/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace repo_Pattern_EFCORE.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
